8c8e1150334551188bf8532dd0268a2584b1fb6e
Mihail Ozun <mihaww@MacBook-Pro-Mihail.local>
2025-05-29 13:07:43 +0300

    first commit

---
+++ hidden camera/hidden_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  hidden_cameraApp.swift
+//  hidden camera
+//
+//  Created by Mihail Ozun on 29.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct hidden_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
